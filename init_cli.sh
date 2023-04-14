b6495fc1d0bd91cb2384e6dd8e247a5c86e62199
Maksym Blinov <m.blinov@it-sprout.org>
2023-04-14 13:58:24 +0300

    add env

--- init_cli.sh
+++ init_cli.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 # Configure WP-CLI
-wp --allow-root --path=/var/www/html core install  --url=localhost --title="MySite" --admin_user=maxim --admin_password=1-qwerty --admin_email=admin@gmail.com
+wp --allow-root --path=/var/www/html core install --url=$URL --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
 
 
-wp plugin install woocommerce 
-wp theme install oceanwp && wp theme activate oceanwp
+wp plugin install contact-form-7 --activate
+wp theme install twentyseventeen && wp theme activate twentyseventeen
 wp theme list
 
